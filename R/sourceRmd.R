1a7a530f7f85f235c93d37687c4805f1d0b9f720
Ben Cresswell <benjamin.cresswell@jcu.edu.au>
2024-03-19 16:43:53 +1000

    Cod eand proj file

---
+++ R/sourceRmd.R
@@ -0,0 +1,14 @@
+# source_rmd
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+sourceRmd = function(file, ...) {
+  tmp_file = tempfile(fileext=".R")
+  on.exit(unlink(tmp_file), add = TRUE)
+  knitr::purl(file, output=tmp_file)
+  source(file = tmp_file, ...)
+}
